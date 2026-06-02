46bee29742e3b7b69f62c40594dc28ff20acdfc9
muhammad-rifqi <muhammad45rifki@gmail.com>
2026-06-02 12:31:14 +0700

    abcdef

--- resources/views/reservasi/result.blade.php
+++ resources/views/reservasi/result.blade.php
@@ -89,7 +89,7 @@
     </div>
 
     <!-- FLOATING BUTTON -->
-    <a href="/reservasi/daftar" class="floating-back">
+    <a href="/reservasi" class="floating-back">
         <i class="bi bi-house-door-fill"></i>
     </a>
 
